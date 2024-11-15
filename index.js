5c9d2f4ebc77225cb929614ed642b3a07284f5cf
Ilham Sidik <ilhamsidikc@gmail.com>
2024-11-15 15:30:32 +0700

    initial ai integration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import all dependencies
-const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
-const pino = require("pino");
+import { makeWASocket, useMultiFileAuthState } from "@whiskeysockets/baileys";
+// const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+import pino from "pino";
+// const pino = require("pino");
 
 // create main function
 async function connectToWhatsApp(){
@@ -45,8 +47,21 @@ async function connectToWhatsApp(){
 
         // to reply the chat
         if (!messages[0].key.fromMe) {
-            const responnse = "Hallo, ini adalah balasan otomatis!";
-            await socket.sendMessage(phone, {text: responnse});
+
+            // const responnse = await query(pesan);
+
+            const pronmt = {
+                question: pesan,
+                overrideConfig: {
+                    systemMessagePrompt: "Anda berbicara dalam bahasa Indonesia yang sopan, ramah, dan mudah dimengerti oleh pasien. Anda adalah seorang dokter yang bekerja di Klinik Watumas di Purwokerto. Tujuan Anda adalah memberikan informasi medis, menjawab pertanyaan kesehatan, dan memberi saran yang berguna untuk menjaga kesehatan pasien. Sebagai dokter, Anda berfokus pada aspek kesehatan secara umum dan dapat memberikan saran, rekomendasi gaya hidup sehat, atau menjelaskan berbagai perawatan medis sederhana sesuai kebutuhan pasien. Anda tidak memberikan diagnosa pasti atau resep obat tanpa pemeriksaan langsung, namun Anda dapat membantu pasien memahami gejala atau keluhan mereka serta kapan sebaiknya menemui dokter secara langsung. Jangan gunakan istilah medis yang rumit kecuali diminta, dan pastikan penjelasan Anda mudah dipahami. Selalu tawarkan dukungan dan kesediaan untuk membantu lebih lanjut. Jika pasien bertanya tentang lokasi klinik atau jadwal praktik, berikan informasi seputar Klinik Watumas."
+                }
+            }
+
+            
+            query(pronmt).then( async (response) => {
+                await socket.sendMessage(phone, {text: response.text});
+                // console.log(response);
+            });
         }
 
         return
@@ -55,6 +70,22 @@ async function connectToWhatsApp(){
 
 // call the function
 connectToWhatsApp();
+
+async function query(data) {
+    const response = await fetch(
+        "http://localhost:3000/api/v1/prediction/7e3316ff-2e33-4ca6-b965-cd11422e05e3",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+
 
 
 
